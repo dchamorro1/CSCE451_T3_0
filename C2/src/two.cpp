3588defe449bec489caf45feff43777011cba752
Daniel Chamorro <danielchamorro.q@gmail.com>
2023-03-07 15:01:01 -0600

    added works cited

--- C2/src/two.cpp
+++ C2/src/two.cpp
@@ -7,6 +7,30 @@
 
 using namespace std;
 
+int main() {
+    // The password is hidden as an array of characters
+    char var1[] = {'\x71', '\x7a', '\x67', '\x6a', '\x78', '\x64', '\x6d', '\x76', '\x6b', '\x6f', '\x6a', '\x6d', '\x00'};
+
+    // Prompt the user to enter the password
+    cout << "Please enter the password: ";
+    string input;
+    cin >> input;
+
+    // Convert the user's input to a character array
+    char input_chars[input.length() + 1];
+    strcpy(input_chars, input.c_str());
+
+    // Check if the input matches the password
+    if (strcmp(input_chars, var1) == 0) {
+        cout << "Correct password. Access granted." << endl;
+    } else {
+        cout << "Incorrect password. Access denied." << endl;
+    }
+
+    return 0;
+}
+
+
 void twenty_one(int firstMatrix[][10], int secondMatrix[][10], int rowFirst, int columnFirst, int rowSecond, int columnSecond)
 {
 	int i, j;
@@ -57,25 +81,22 @@ void multiplyMatrices(int firstMatrix[][10], int secondMatrix[][10], int mult[][
 	}
 }
 
-int main() {
-    // The password is hidden as an array of characters
-    char var1[] = {'\x71', '\x7a', '\x67', '\x6a', '\x78', '\x64', '\x6d', '\x76', '\x6b', '\x6f', '\x6a', '\x6d', '\x00'};
+void display(int mult[][10], int rowFirst, int columnSecond)
+{
+	int i, j;
 
-    // Prompt the user to enter the password
-    cout << "Please enter the password: ";
-    string input;
-    cin >> input;
-
-    // Convert the user's input to a character array
-    char input_chars[input.length() + 1];
-    strcpy(input_chars, input.c_str());
-
-    // Check if the input matches the password
-    if (strcmp(input_chars, var1) == 0) {
-        cout << "Correct password. Access granted." << endl;
-    } else {
-        cout << "Incorrect password. Access denied." << endl;
-    }
-
-    return 0;
+	cout << "Output Matrix:" << endl;
+	for(i = 0; i < rowFirst; ++i)
+	{
+		for(j = 0; j < columnSecond; ++j)
+		{
+			cout << mult[i][j] << " ";
+			if(j == columnSecond - 1)
+				cout << endl << endl;
+		}
+	}
 }
+
+
+// Works Cited:
+// https://www.programiz.com/cpp-programming/examples/matrix-multiplication-function

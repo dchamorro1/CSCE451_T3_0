294af3adad947c507bf3a639934e634b89454c0a
Daniel Chamorro <danielchamorro.q@gmail.com>
2023-03-31 18:13:06 -0500

    attempting to feed in a file and get a response from virustotal

--- C3/virusTotal.py
+++ C3/virusTotal.py
@@ -6,7 +6,33 @@
 #@toolbar
 # Running binary through VirusTotal
 
+import requests
+
 
 #TODO Add User Code Here
 
-print("Starting Script")
+def main():
+    print("Starting Script")
+
+    # TODO: get file with ghidra function
+
+
+
+    # TODO: Feeding file into virustotal
+
+
+    url = "https://www.virustotal.com/api/v3/files"
+
+    files = {"file": ("wordle", open("wordle", "rb"), "application/octet-stream")}
+    headers = {
+        "accept": "application/json",
+        "x-apikey": "6a90acebb6999746d1649a244d982d162cf87488440afc942d46a99abe4ee98a"
+    }
+
+    response = requests.post(url, files=files, headers=headers)
+
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()

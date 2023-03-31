6ea51a0aaaf42afab3955d04e979b9a27ad36ed7
Daniel Chamorro <danielchamorro.q@gmail.com>
2023-03-31 17:50:21 -0500

    added virusTotal.py

---
+++ C3/virusTotal.py
@@ -0,0 +1,12 @@
+#TODO write a description for this script
+#@author
+#@category Memory
+#@keybinding
+#@menupath Tools.Virus Total
+#@toolbar
+# Running binary through VirusTotal
+
+
+#TODO Add User Code Here
+
+print("Starting Script")

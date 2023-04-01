30d13d8694a802ff2d4b0bffd1d48c03d84ede26
Caleb <103637020+calebci@users.noreply.github.com>
2023-03-31 19:23:09 -0500

    extract out file with ghidra functions

--- C3/virusTotal.py
+++ C3/virusTotal.py
@@ -8,7 +8,6 @@
 
 # import requests
 
-
 #TODO Add User Code Here
 
 import requests
@@ -31,32 +30,6 @@ def main():
     print("Starting Script")
 
     # TODO: get file with ghidra function
-
-    # specify the name of the project and program
-    project_name = "MyProject"
-    program_name = "MyProgram"
-
-    # get the project and program objects
-    project_mgr = Project.getProjects()
-    project = None
-    for p in project_mgr:
-        if p.getName() == project_name:
-            project = p
-            break
-
-    if project is None:
-        print(f"Could not find project {project_name}")
-        exit()
-
-    program = project.getProgram(program_name)
-    if program is None:
-        print(f"Could not find program {program_name}")
-        exit()
-
-    # extract the .out file
-    out_path = os.path.join(os.path.dirname(program.getExecutablePath()), program.getName() + ".out")
-    with open(out_path, "wb") as f:
-    f.write(program.getMemory().getBytes())
 
     # TODO: Feeding file into virustotal
     url = "https://www.virustotal.com/api/v3/files"

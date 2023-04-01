750118f0b1fab9e6f256daf666b95fe5b92aa2db
Daniel Chamorro <danielchamorro.q@gmail.com>
2023-03-31 19:03:08 -0500

    got virustotal to print details about wordle.out

--- C3/virusTotal.py
+++ C3/virusTotal.py
@@ -11,6 +11,20 @@ import requests
 
 #TODO Add User Code Here
 
+import requests
+
+# url = "https://www.virustotal.com/api/v3/files"
+#
+# files = {"file": ("wordle.out", open("wordle.out", "rb"), "application/octet-stream")}
+# headers = {
+#     "accept": "application/json",
+#     "x-apikey": "6a90acebb6999746d1649a244d982d162cf87488440afc942d46a99abe4ee98a"
+# }
+#
+# response = requests.post(url, files=files, headers=headers)
+#
+# print(response.text)
+
 def main():
     print("Starting Script")
 
@@ -19,17 +33,24 @@ def main():
 
 
     # TODO: Feeding file into virustotal
-
-
     url = "https://www.virustotal.com/api/v3/files"
 
-    files = {"file": ("wordle", open("wordle", "rb"), "application/octet-stream")}
+    files = {"file": ("wordle.out", open("wordle.out", "rb"), "application/octet-stream")}
+
     headers = {
         "accept": "application/json",
         "x-apikey": "6a90acebb6999746d1649a244d982d162cf87488440afc942d46a99abe4ee98a"
     }
 
     response = requests.post(url, files=files, headers=headers)
+
+    print(response.text)
+
+    analysis_url = response.json()["data"]["links"]["self"]
+
+    print(analysis_url)
+
+    response = requests.get(analysis_url, headers=headers)
 
     print(response.text)
 

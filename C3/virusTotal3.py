de057ff48a784dc8e715e07e7d08075e6fa4c9d2
Daniel Chamorro <danielchamorro.q@gmail.com>
2023-04-02 13:24:53 -0500

    trying urllib

--- C3/virusTotal3.py
+++ C3/virusTotal3.py
@@ -5,60 +5,30 @@
 # @toolbar
 # Running binary through VirusTotal
 
-# TODO Add User Code Here
-
-# import requests
-# import os
-from ghidra.framework import Project
-# import os
-# import ghidra.app.util.bin.exporter.ExporterException as ExporterException
-# import ghidra.app.util.bin.exporter.RawExporter as RawExporter
-
 import urllib
 import urllib2
-import json
+
 
 def main():
+    print("Starting Script")
 
+    ### Changing old code to use urllib/urllib2 ###########################################################
 
-    # *done - Feeding file into virustotal
+    state = getState()
+    currentProgram = state.getCurrentProgram()
+    location = currentProgram.getExecutablePath()
+
     url = "https://www.virustotal.com/api/v3/files"
 
-    files = {"file": (out_file_path, open(out_file_path, "rb"), "application/octet-stream")}
+    files = {"file": (currentProgram, open(location, "rb"), "application/octet-stream")}
 
     headers = {
         "accept": "application/json",
         "x-apikey": "6a90acebb6999746d1649a244d982d162cf87488440afc942d46a99abe4ee98a"
     }
 
-    response = requests.post(url, files=files, headers=headers)
-
-    print(response.text)
-
-    analysis_url = response.json()["data"]["links"]["self"]
-
-    print(analysis_url)
-
-    response = requests.get(analysis_url, headers=headers)
-
-    print("----Getting response from analysis_url----")
-    print(response.text)
-
-
-    ### Changing the above code to use urllib/urllib2 ###########################################################
-
-    url = "https://www.virustotal.com/api/v3/files"
-
-    files = {"file": (out_file_path, open(out_file_path, "rb"), "application/octet-stream")}
-
-    headers = {
-        "accept": "application/json",
-        "x-apikey": "6a90acebb6999746d1649a244d982d162cf87488440afc942d46a99abe4ee98a"
-    }
-
-
-
-    requests = urllib2(Request(url))
+    # requests = urllib2(Request(url))
+    requests = urllib.request.urlopen(url)
 
     # data = urllib.urlencode(headers)
 
@@ -67,6 +37,8 @@ def main():
 
     requests.add_data(files)
 
+    html = requests.read()
+    print(html)
 
 
 if __name__ == "__main__":

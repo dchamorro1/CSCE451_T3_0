7a780bf3589f46f06e85d84de80254ad4612b975
hollow-owl <71401395+hollow-owl@users.noreply.github.com>
2023-04-03 00:01:56 -0500

    API requests

--- C3/virusTotal.py
+++ C3/virusTotal.py
@@ -1,54 +1,49 @@
 # @author
-# @category Memory
+# @category _NEW_
 # @keybinding
 # @menupath Tools.Virus Total
 # @toolbar
 # Running binary through VirusTotal
 
-# TODO Add User Code Here
-
-import requests
+#TODO Add User Code Here
+import urllib2
 import os
-from ghidra.framework import Project
-import os
-import ghidra.app.util.bin.exporter.ExporterException as ExporterException
-import ghidra.app.util.bin.exporter.RawExporter as RawExporter
+import subprocess
+import shutil
+import json
 
-
+key =  '6a90acebb6999746d1649a244d982d162cf87488440afc942d46a99abe4ee98a'
 def main():
     print("Starting Script")
-
-    # TODO: get file with ghidra function
-
 
     state = getState()
     currentProgram = state.getCurrentProgram()
     name = currentProgram.getName()
     location = currentProgram.getExecutablePath()
 
-
+    # Submit the file
     url = "https://www.virustotal.com/api/v3/files"
+    path = '/tmp/{0}'.format(name)
+    shutil.copyfile(location, path)
+    command = "curl --request POST \
+     --url {url} \
+     --header 'accept: application/json' \
+     --header 'content-type: multipart/form-data' \
+     --header 'x-apikey: {key}' \
+     --form file=@{file}".format(**{'url': url, 'key': key,'file':path})
+    scan_result = subprocess.check_output(command,shell=True)
+    print(scan_result)
+    os.remove(path)
 
+    # Actual data
+    scan_url = json.loads(scan_result)['data']['links']['self']
 
-    files = {"file": (currentProgram, open(location, "rb"), "application/octet-stream")}
-
-    headers = {
-        "accept": "application/json",
-        "x-apikey": "6a90acebb6999746d1649a244d982d162cf87488440afc942d46a99abe4ee98a"
-    }
-
-    response = requests.post(url, files=files, headers=headers)
-
-    print(response.text)
-
-    analysis_url = response.json()["data"]["links"]["self"]
-
-    print(analysis_url)
-
-    response = requests.get(analysis_url, headers=headers)
-
-    print("----Getting response from analysis_url----")
-    print(response.text)
+    command2 = "curl --request GET \
+        --url {url} \
+        --header 'x-apikey: {key}'".format(**{'url': scan_url, 'key': key})
+    result = subprocess.check_output(command2,shell=True)
+    result = json.loads(result)
+    print result
 
     # print("--- Getting data.links.sels---")
     # response = requests.get(response.json()["data"]["links"]["self"], headers=headers)

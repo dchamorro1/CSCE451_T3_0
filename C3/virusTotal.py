fb994296633146061befe8954b47e000d28ad852
hollow-owl <71401395+hollow-owl@users.noreply.github.com>
2023-04-04 19:53:51 -0500

    Fix VT

--- C3/virusTotal.py
+++ C3/virusTotal.py
@@ -11,18 +11,25 @@ import os
 import subprocess
 import shutil
 import json
+import sys
+import hashlib
+import time
 
 key =  '6a90acebb6999746d1649a244d982d162cf87488440afc942d46a99abe4ee98a'
-def main():
-    print("Starting Script")
+url = "https://www.virustotal.com/api/v3/"
+def get_results(scan_url):
+    command2 = "curl --request GET \
+        --url {url} \
+        --header 'accept: application/json' \
+        --header 'x-apikey: {key}'".format(**{'url': scan_url, 'key': key})
+    
+    print(command2)
+    result = subprocess.check_output(command2,shell=True)
+    print(result)
+    result = json.loads(result)
+    return result
 
-    state = getState()
-    currentProgram = state.getCurrentProgram()
-    name = currentProgram.getName()
-    location = currentProgram.getExecutablePath()
-
-    # Submit the file
-    url = "https://www.virustotal.com/api/v3/files"
+def submit_file(name, location): # location includes name
     path = '/tmp/{0}'.format(name)
     shutil.copyfile(location, path)
     command = "curl --request POST \
@@ -30,24 +37,65 @@ def main():
      --header 'accept: application/json' \
      --header 'content-type: multipart/form-data' \
      --header 'x-apikey: {key}' \
-     --form file=@{file}".format(**{'url': url, 'key': key,'file':path})
+     --form file=@{file}".format(**{'url': url+'files', 'key': key,'file':path})
     scan_result = subprocess.check_output(command,shell=True)
     print(scan_result)
     os.remove(path)
+    return json.loads(scan_result)
 
-    # Actual data
-    scan_url = json.loads(scan_result)['data']['links']['self']
+    
+def main():
+    print("Starting Script")
 
-    command2 = "curl --request GET \
-        --url {url} \
-        --header 'x-apikey: {key}'".format(**{'url': scan_url, 'key': key})
-    result = subprocess.check_output(command2,shell=True)
-    result = json.loads(result)
-    print result
+    state = getState()
+    currentProgram = state.getCurrentProgram()
+    name = currentProgram.getName()
+    location = currentProgram.getExecutablePath()
+    hash_sha = currentProgram.getExecutableSHA256()
 
-    # print("--- Getting data.links.sels---")
-    # response = requests.get(response.json()["data"]["links"]["self"], headers=headers)
-    # print(response.text)
+    
+    # name = 'test1.dat'
+    # location = sys.argv[1] + name
+    # h = hashlib.sha256()
+    # with open(location, 'rb', buffering=0) as f:
+    #     h.update(f.read())
+    # hash_sha = h.hexdigest()
+    # print(hash_sha)
+
+    # Try hash
+    result = get_results(url + 'files/' + hash_sha)
+    # Upload file if that doesn't work
+    if('error' in result):
+        print('File not found')
+        print('Prompt to upload file?')
+        # Submit the file
+        scan_result = submit_file(name,location)
+        print(json.dumps(scan_result, indent=4, sort_keys=True))
+
+        # Actual data
+        scan_url = scan_result['data']['links']['self']
+        result = None
+
+        # Takes time to process
+        # try multiple times with exp backoff
+        for i in range(6): # upto 2^6 or 64 seconds wait
+            result = get_results(scan_url)
+            if(result['data']['attributes']['status'] == 'completed'):
+                break
+            print("trying again", result['data']['attributes']['status'])
+            time.sleep(2**i) # exponential back-off 2, 4, 8, 16 sec
+        if result == None:
+            print('Could not upload file please try again')
+            return -1
+
+        # file and hash results are different but contain the same information
+        # get hash result
+        result = get_results(url + 'files/' + hash_sha)
+
+    print('Display output')
+    print(json.dumps(result, indent=4, sort_keys=True))
+
+    # TODO: do stuff with output
 
 
 if __name__ == "__main__":
